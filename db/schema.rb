d0344b54fc7778fd4347b32792f560a74b71f4ad
Ben <benpong89@gmail.com>
2018-09-12 13:06:51 -0400

    update color

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_06_172214) do
+ActiveRecord::Schema.define(version: 2018_09_12_161730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
