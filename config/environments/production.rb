4faa674300955d4aeee24667567c9ec14b8370b2
Ben <benpong89@gmail.com>
2018-09-15 16:03:45 -0400

    troubleshoot heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://slangg.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://slangg.herokuapp.com', 'http://slangg.herokuapp.com']
 end
