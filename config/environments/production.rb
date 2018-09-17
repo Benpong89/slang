642575383b01e1256ac7e181505e1309809515b1
Ben <benpong89@gmail.com>
2018-09-17 09:39:19 -0400

    troubleshoot heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,8 +45,8 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.web_socket_server_url = "wss://slangg.herokuapp.com/cable"
-  config.action_cable.url = 'wss://slangg.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'http://slangg.herokuapp.com', 'http://slangg.herokuapp.com', /http:\/\/slangg.herokuapp.*/ ]
+  # config.action_cable.url = 'wss://slangg.herokuapp.com/cable'
+  # config.action_cable.allowed_request_origins = [ 'http://slangg.herokuapp.com', 'http://slangg.herokuapp.com', /http:\/\/slangg.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
